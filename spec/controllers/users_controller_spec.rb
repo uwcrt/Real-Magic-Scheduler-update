2cf76cde333ce2bcabec77b3cddc04f0939ad396
Justin Vanderheide <justin@justinvanderheide.com>
2010-12-29 20:48:20 -0500

    Fixed error messages

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,6 +36,7 @@ describe UsersController do
   end
 	
   describe "GET 'new'" do
+    
     it "should be successful" do
       get 'new'
       response.should be_success
@@ -44,6 +45,27 @@ describe UsersController do
     it "should have the right title" do
     	get 'new'
     	response.should have_selector("title", :content => "Sign up")
+    end
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='text']")
+    end
+
+    it "should have a password confirmation field" do
+    do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='text']")
     end
   end
   
