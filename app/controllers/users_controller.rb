4e9682e412e48a2b27625aefd0df7c1ad2425d4c
Justin Vanderheide <justin@justinvanderheide.com>
2010-12-29 20:43:37 -0500

    Creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome to the sample application!"
+			redirect_to @user
+		else
+			@title = "Sign up"
+			@user.password = "s"
+			@user.password_confirmation = ""
+			render 'new'
+		end
+	end
+
   def new
   	@title = "Sign up"
+  	@user = User.new
   end
 	
 	def show
