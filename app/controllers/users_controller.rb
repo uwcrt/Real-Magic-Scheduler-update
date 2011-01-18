a75fc34fd18907bea1d8ef91e8a3c74f17588328
Justin Vanderheide <justin@justinvanderheide.com>
2011-01-18 14:25:30 -0500

    Displaying shifts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,8 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@title = @user.name
+		@past_shifts = @user.shifts.select {|shift| shift.start <= DateTime.now }
+    @current_shifts = @user.shifts.select {|shift| shift.start > DateTime.now }
 	end
 
 	private
