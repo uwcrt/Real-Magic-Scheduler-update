e011d9fb212b92263997446e5a5cbf3bb81b0ce3
Justin Vanderheide <justin@justinvanderheide.com>
2012-05-15 20:46:42 -0400

    4 hours

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -33,11 +33,11 @@ class Shift < ActiveRecord::Base
   belongs_to :shift_type
 
   def self.current
-    Shift.where("start >= ?", DateTime.now - 5.hours)
+    Shift.where("start >= ?", DateTime.now - 4.hours)
   end
   
   def self.past
-    Shift.where("start < ?", DateTime.now - 5.hours)
+    Shift.where("start < ?", DateTime.now - 4.hours)
   end
   
   def self.available
