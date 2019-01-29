a716540389899baa4b3bc32b2e5213588b1bf429
Benjamin Wilkins <benjamin.wilkins99@gmail.com>
2019-01-29 13:37:25 -0500

    Change FactoryBot static attributes to dynamic attributes

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
   factory :user do
-    first_name            "Justin"
-    last_name             "Vanderheide"
-    username              "jvand"
+    first_name            { "Justin" }
+    last_name             { "Vanderheide" }
+    username              { "jvand" }
   end
 
   factory :shift do 
-    name      "Example Shift"
-    start     DateTime.now
-    finish    DateTime.now + 5.hours
-    location  "Student Life Center"
-    shift_type_id 1
+    name      { "Example Shift" }
+    start     { DateTime.now }
+    finish    { DateTime.now + 5.hours }
+    location  { "Student Life Center" }
+    shift_type_id { 1 }
   end
 
   factory :shift_type do 
-    name                  "regular"
-    primary_requirement   10
-    secondary_requirement 12
+    name                  { "regular" }
+    primary_requirement   { 10 }
+    secondary_requirement { 12 }
   end
 
   factory :email do |n|
