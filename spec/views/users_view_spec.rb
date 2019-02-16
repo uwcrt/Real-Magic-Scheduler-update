fdafd59be2bb6f554818345620ee3bf9f17bb8dc
Benjamin Wilkins <benjamin.wilkins99@gmail.com>
2019-02-16 16:23:26 -0500

    [#32] Initialize filterrific in view tests

--- spec/views/users_view_spec.rb
+++ spec/views/users_view_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe "users/index" do
+  before(:each) do
+    allow_any_instance_of(ActionView::TestCase::TestController).to receive(:controller_name).and_return('users')
+    @filterrific = Filterrific::ParamSet.new(User, {})
+  end
+
   before do
     assign(:users, [create(:user), create(:user, disabled: true)])
     assign(:shift_types, [create(:shift_type)])
